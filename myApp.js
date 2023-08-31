a4d4f4f0e69f8b59de196beb7f323080d6913f3f
M. Waqar ul Amin <mwaqarulamin@gmail.com>
2023-08-31 11:32:28 +0500

    fixed mongo

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
